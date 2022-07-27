e0d7f18ed8b9b65b5939292b8f643457c6221674
LuciusRen <renhongfeiasd@gmail.com>
2022-07-27 17:15:26 +0800

    [WN] 更新第二章-选择排序的示例代码

--- main.py
+++ main.py
@@ -19,8 +19,28 @@ def binary_search(list, item):
             low = mid + 1
     return None
 
+# 第二章 选择排序
+
+# 2.3 选择排序
+def findSmallesetIndex(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if (arr[i] < smallest):
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+def selection_sort(arr):
+    sorted_arr = []
+    for i in range(len(arr)):
+        smallest = findSmallesetIndex(arr)
+        sorted_arr.append(arr.pop(smallest))
+    return sorted_arr
+
 if __name__ == '__main__':
-    my_list = [1,3,5,7,9]
-    print(str(binary_search(my_list, 3))) # => 1
-    print(binary_search(my_list, -1)) # => None
-    print('测试python-Lucius')
+    my_list = [1,33,7,93,5]
+    print(str(selection_sort(my_list))) # => 1
+    #print(str(binary_search(my_list, 3))) # => 1
+    #print(binary_search(my_list, -1)) # => None
+    #print('测试python-Lucius')
