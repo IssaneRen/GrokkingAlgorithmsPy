2db1d6afff6acaac90029a33b38d34d0693c55c2
hongfei_ren <hongfei_ren@intsig.net>
2022-07-28 14:58:28 +0800

    [WN] 4.1 分而治之的sum方法

--- main.py
+++ main.py
@@ -38,9 +38,23 @@ def selection_sort(arr):
         sorted_arr.append(arr.pop(smallest))
     return sorted_arr
 
+# 第四章 快速排序
+
+# 4.1 分而治之
+# 习题1 编写sum函数的代码
+def sum41(num, arr):
+    l = len(arr)
+    if (l == 0):
+        return num
+    if (l == 1):
+        return num + arr[0]
+    arr.pop(0)
+    return sum41(num+arr[0], arr)
+    
+
 if __name__ == '__main__':
     my_list = [1,33,7,93,5]
-    print(str(selection_sort(my_list))) # => 1
+    print(str(sum41(0, my_list))) # => 1
     #print(str(binary_search(my_list, 3))) # => 1
     #print(binary_search(my_list, -1)) # => None
     #print('测试python-Lucius')
